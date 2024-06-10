eec742424d5b4fd5e479aa97961cc48a4f152003
awzmb <bundschuh.dennis@gmail.com>
2024-06-10 22:12:34 +0200

    fix(terraform-gcp-project): changing variable input to fit requirements

--- terraform-gcp-project/main.tf
+++ terraform-gcp-project/main.tf
@@ -6,14 +6,23 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 5.25.0, < 6"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
   }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 8
 }
 
 resource "google_project" "project" {
   #checkov:skip=CKV2_GCP_5:not a professional setup
   #checkov:skip=CKV_GCP_27:not a professional setup
   name       = var.display_name
-  project_id = var.display_name
+  project_id = var.project_id
 
   auto_create_network = true
 }
