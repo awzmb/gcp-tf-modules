bce0e6ce5ca6f11e2b9d56a031b01ad3406cfd63
awzmb <bundschuh.dennis@gmail.com>
2024-06-10 21:59:19 +0200

    feat(terraform-gcp-project): added project_id variable

--- terraform-gcp-project/variables.tf
+++ terraform-gcp-project/variables.tf
@@ -2,3 +2,8 @@ variable "display_name" {
   description = "Display name of the project."
   type        = string
 }
+
+variable "project_id" {
+  description = "Identifier of the project."
+  type        = string
+}
