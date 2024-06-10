1ee71c0311eeb59867a171a153d824f8a3bb1e61
awzmb <bundschuh.dennis@gmail.com>
2024-06-10 22:55:53 +0200

    feat(terraform-gcp-cloud-domain-registration): added validation for input variables

--- terraform-gcp-cloud-domain-registration/variables.tf
+++ terraform-gcp-cloud-domain-registration/variables.tf
@@ -1,6 +1,11 @@
 variable "domain_name" {
   description = "Name of the domain to be registered."
   type        = string
+
+  validation {
+    condition     = can(regex("[a-zA-Z0-9-]{1,63}\\.([a-zA-Z]{2,})", var.domain_name))
+    error_message = "The 'domain_name' variable must be a valid domain name."
+  }
 }
 
 variable "pricing_currency_code" {
@@ -22,6 +27,11 @@ variable "labels" {
 variable "region_code" {
   description = "Registrant contact region code."
   type        = string
+
+  validation {
+    condition     = length(var.region_code) == 2
+    error_message = "The variable must be a 2-letter country code."
+  }
 }
 
 variable "organization" {
@@ -47,14 +57,29 @@ variable "address" {
 variable "administrative_area" {
   description = "Registrant contact administrative area. Two-letter code g.e. CA, BE."
   type        = string
+
+  validation {
+    condition     = length(var.administrative_area) == 2
+    error_message = "The administrative_area variable must be a 2-letter country code."
+  }
 }
 
 variable "email_address" {
   description = "Registrant contact email."
   type        = string
+
+  validation {
+    condition     = can(regex("^\\S+@\\S+$", var.email_address))
+    error_message = "The email_address variable must be a valid email address."
+  }
 }
 
 variable "phone_number" {
   description = "Registrant contact phone number."
   type        = string
+
+  validation {
+    condition     = can(regex("^\\+?\\d+$", var.phone_number))
+    error_message = "The phone_number variable must be a valid phone number."
+  }
 }
