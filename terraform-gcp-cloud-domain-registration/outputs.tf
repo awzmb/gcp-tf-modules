ed1605cf1a1323d62cb74db24980563d24dd8d7a
awzmb <bundschuh.dennis@gmail.com>
2024-06-11 10:51:34 +0200

    fix(terraform-gcp-cloud-domain-registration): fixed output values

--- terraform-gcp-cloud-domain-registration/outputs.tf
+++ terraform-gcp-cloud-domain-registration/outputs.tf
@@ -1,20 +1,20 @@
 output "domain_name" {
   description = "The domain name of the registration."
-  value       = google_clouddomains_registration.default.domain_name
+  value       = google_clouddomains_registration.registration.domain_name
 }
 
 output "create_time" {
   description = "The creation time of the registration."
-  value       = google_clouddomains_registration.default.create_time
+  value       = google_clouddomains_registration.registration.create_time
 }
 
 output "expire_time" {
   description = "The expiration time of the registration."
-  value       = google_clouddomains_registration.default.expire_time
+  value       = google_clouddomains_registration.registration.expire_time
 }
 
 output "state" {
   description = "The state of the registration."
-  value       = google_clouddomains_registration.default.state
+  value       = google_clouddomains_registration.registration.state
 }
 
