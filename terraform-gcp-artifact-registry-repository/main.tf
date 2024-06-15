7c15a58a44044d871899b61aa488f329bfa55ad8
bawzm <bundschuh.dennis@gmail.com>
2024-06-15 13:05:18 +0200

    fix(terraform-gcp-artifact-registry-repository): removed project argument

--- terraform-gcp-artifact-registry-repository/main.tf
+++ terraform-gcp-artifact-registry-repository/main.tf
@@ -27,7 +27,6 @@ resource "google_project_service" "enable_artifact_registry" {
 resource "google_artifact_registry_repository" "repository" {
   description = "${var.format} artifact repository."
 
-  project       = data.google_project.project.number
   location      = var.location
   format        = var.format
   repository_id = var.repository_id
