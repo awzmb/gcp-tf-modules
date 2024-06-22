92560b33c2f8e9ec0beea4b51a11a82b208dfe4b
bawzm <bundschuh.dennis@gmail.com>
2024-06-22 23:29:13 +0200

    fix(terraform-gcp-gke-cluster): use latest istio version

--- terraform-gcp-gke-cluster/locals.tf
+++ terraform-gcp-gke-cluster/locals.tf
@@ -1,7 +1,9 @@
 locals {
   gke_cluster_name = "gke-${var.name}"
 
-  istio_version          = "1.22.1"
+  #istio_version          = "1.22.1"
+  istio_version = "latest"
+
   internal_subnet_cidr   = "10.0.0.0/24"
   master_ipv4_cidr_block = "172.16.0.16/28"
   proxy_only_ipv4_cidr   = "11.129.0.0/23"
