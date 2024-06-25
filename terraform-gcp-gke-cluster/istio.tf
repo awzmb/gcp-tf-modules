86d334770d4c7a9dd0e6a96e3006deb8c774bfe9
bawzm <bundschuh.dennis@gmail.com>
2024-06-25 23:07:23 +0200

    fix(terraform-gcp-gke-cluster): trying to force service to create NEG for every zone in the region

--- terraform-gcp-gke-cluster/istio.tf
+++ terraform-gcp-gke-cluster/istio.tf
@@ -70,7 +70,8 @@ service:
       protocol: TCP
       targetPort: 443
   annotations:
-    cloud.google.com/neg: '{"exposed_ports": {"80":{"name": "${local.istio_ingress_gateway_endpoint_group_http}"}}, {"443":{"name": "${local.istio_ingress_gateway_endpoint_group_https}"}}}'
+    cloud.google.com/neg: '{"exposed_ports": {"80":{"name": "${local.istio_ingress_gateway_endpoint_group}"}}}'
+    cloud.google.com/neg-affinity: '{"zones": ["${var.region}-a", "${var.region}-b", "${var.region}-c"]}'
   loadBalancerIP: ""
   loadBalancerSourceRanges: []
   externalTrafficPolicy: ""
