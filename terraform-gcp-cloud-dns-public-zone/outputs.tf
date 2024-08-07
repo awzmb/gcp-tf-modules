34a58e5d5cb0a485324bfb984112aadc54fc9f39
Dennis Bundschuh <dennis.bundschuh@metamorphant.de>
2024-08-07 17:16:43 +0200

    feat(terraform-gcp-cloud-dns-public-zone): added dns_name output to use as dependency for cloud domain registration

--- terraform-gcp-cloud-dns-public-zone/outputs.tf
+++ terraform-gcp-cloud-dns-public-zone/outputs.tf
@@ -12,3 +12,8 @@ output "name_servers" {
   description = "Delegate your managed_zone to these virtual name servers; defined by the server."
   value       = google_dns_managed_zone.zone.name_servers
 }
+
+output "dns_name" {
+  description = "The DNS name for the managed zone (g.e. private.example.com)."
+  value       = var.dns_zone
+}
