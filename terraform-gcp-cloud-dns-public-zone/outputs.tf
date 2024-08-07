b1b9c21e50c4aade4a0e1ca7c6dbf06eb9ff4948
Dennis Bundschuh <dennis.bundschuh@metamorphant.de>
2024-08-07 17:01:02 +0200

    feat(terraform-gcp-cloud-dns-public-zone): added output name_servers

--- terraform-gcp-cloud-dns-public-zone/outputs.tf
+++ terraform-gcp-cloud-dns-public-zone/outputs.tf
@@ -7,3 +7,8 @@ output "zone_name" {
   description = "The name of the DNS zone."
   value       = google_dns_managed_zone.zone.name
 }
+
+output "name_servers" {
+  description = "Delegate your managed_zone to these virtual name servers; defined by the server."
+  value       = google_dns_managed_zone.zone.name_servers
+}
