a07b21b62430461e9af85b7aaf1b42bb2f143c5f
Dennis Bundschuh <dennis.bundschuh@metamorphant.de>
2024-08-10 12:06:21 +0200

    fix(terraform-gcp-gke-autopilot-cluster): code cleanup

--- terraform-gcp-gke-autopilot-cluster/external-dns.tf
+++ terraform-gcp-gke-autopilot-cluster/external-dns.tf
@@ -1,45 +1,8 @@
-# deploy istio via submodule to tackle the data object dependency
-# problem on the first deployment of the cluster.
-#module "external_dns" {
-#source = "./submodules/external-dns"
-
-#cluster_name = google_container_cluster.default.name
-#region       = var.region
-#project_id   = var.project_id
-
-#external_dns_version = "0.14.2"
-
-#dns_zone_name = var.dns_zone_name
-#}
-
 resource "google_service_account" "external_dns" {
   account_id   = "external-dns"
   display_name = "external-dns Service Account"
   project      = var.project_id
 }
-
-## custom role to manage DNS records in Cloud DNS
-#resource "google_project_iam_custom_role" "manage_dns_records" {
-#description = "Allows managing DNS records in Cloud DNS."
-#permissions = [
-#"dns.resourceRecordSets.list",
-#"dns.resourceRecordSets.create",
-#"dns.resourceRecordSets.delete",
-#"dns.resourceRecordSets.update",
-#"dns.changes.get",
-#"dns.changes.create",
-#"dns.managedZones.list",
-#]
-#project = var.project_id
-#role_id = "ManageDNSRecords${random_id.random_role_id_suffix.hex}"
-#title   = "Manage DNS records"
-#}
-
-#resource "google_project_iam_binding" "external_dns" {
-#project = var.project_id
-#role    = google_project_iam_custom_role.manage_dns_records.id
-#members = [google_service_account.external_dns.member]
-#}
 
 resource "google_project_iam_binding" "external_dns" {
   project = var.project_id
@@ -241,34 +204,3 @@ resource "kubernetes_deployment" "external_dns" {
     kubernetes_cluster_role_binding.external_dns,
   ]
 }
-
-#resource "helm_release" "external_dns" {
-#name       = "external-dns"
-#repository = "https://kubernetes-sigs.github.io/external-dns"
-#chart      = "external-dns"
-#version    = local.external_dns_version
-
-#namespace = "external-dns"
-
-#dependency_update = true
-#create_namespace  = true
-#wait_for_jobs     = true
-#atomic            = true
-
-#values = [
-#<<EOF
-#extraArgs:
-#- --source=ingress
-#- --source=service
-#- --source=istio-gateway
-#- --source=virtualservice
-#- --domain-filter=${data.google_dns_managed_zone.dns_zone.dns_name}
-#- --provider=google
-#- --google-project=${var.project_id}
-#- --registry=txt
-#- --log-format=json
-#- --policy=upsert-only
-#- --txt-owner-id=external-dns
-#EOF
-#]
-#}
--- terraform-gcp-gke-autopilot-cluster/istio.tf
+++ terraform-gcp-gke-autopilot-cluster/istio.tf
@@ -1,17 +1,3 @@
-# deploy istio via submodule to tackle the data object dependency
-# problem on the first deployment of the cluster.
-#module "istio" {
-#source = "./submodules/istio"
-
-#cluster_name = google_container_cluster.default.name
-#region       = var.region
-
-#istio_version = local.istio_version
-
-#http_backend_service_name  = local.istio_ingress_gateway_endpoint_group_http_backend_service
-#https_backend_service_name = local.istio_ingress_gateway_endpoint_group_https_backend_service
-#}
-
 resource "helm_release" "istio_base" {
   name       = "istio-base"
   repository = "https://istio-release.storage.googleapis.com/charts"
