66da13e2386858286cecf942083a8e41757f335b
Dennis Bundschuh <bundschuh.dennis@gmail.com>
2024-08-17 14:24:37 +0200

    fix(terraform-gcp-gke-autopilot-cluster): point the dns entry to the compute address

--- terraform-gcp-gke-autopilot-cluster/dns.tf
+++ terraform-gcp-gke-autopilot-cluster/dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_record_set" "record" {
-  name         = "${local.gke_cluster_name}-static-ip"
+  name         = data.google_dns_managed_zone.dns_zone.dns_name
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.dns_zone.name
