a7131c070111ccc8e2ce4e508128cb79dfd62418
Dennis Bundschuh <bundschuh.dennis@gmail.com>
2024-08-17 14:14:19 +0200

    fix(terraform-gcp-gke-autopilot-cluster): removed abundant dots

--- terraform-gcp-gke-autopilot-cluster/dns.tf
+++ terraform-gcp-gke-autopilot-cluster/dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_record_set" "http_record" {
-  name         = "${data.google_dns_managed_zone.dns_zone.dns_name}."
+  name         = data.google_dns_managed_zone.dns_zone.dns_name
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.dns_zone.name
@@ -12,7 +12,7 @@ resource "google_dns_record_set" "http_record" {
 }
 
 resource "google_dns_record_set" "http_wildcard_record" {
-  name         = "*.${data.google_dns_managed_zone.dns_zone.dns_name}."
+  name         = "*.${data.google_dns_managed_zone.dns_zone.dns_name}"
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.dns_zone.name
@@ -25,7 +25,7 @@ resource "google_dns_record_set" "http_wildcard_record" {
 }
 
 resource "google_dns_record_set" "https_record" {
-  name         = "${data.google_dns_managed_zone.dns_zone.dns_name}."
+  name         = data.google_dns_managed_zone.dns_zone.dns_name
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.dns_zone.name
@@ -38,7 +38,7 @@ resource "google_dns_record_set" "https_record" {
 }
 
 resource "google_dns_record_set" "https_wildcard_record" {
-  name         = "*.${data.google_dns_managed_zone.dns_zone.dns_name}."
+  name         = "*.${data.google_dns_managed_zone.dns_zone.dns_name}"
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.dns_zone.name
